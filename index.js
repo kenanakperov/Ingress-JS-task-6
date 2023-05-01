4513a0e681b7f82125225b7ce69e086aae40ffc6
kenanakperov <kenanakperov5gmail.com>
2023-05-01 22:40:08 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// ! month, day, year with "/" version 1
+
+// const d = new Date().toLocaleDateString("az-AZ")
+// let arr = d.split("-")
+// let result = arr[1]+"/"+arr[2]+"/"+arr[0]
+
+// console.log(result)
+
+// ! month, day, year with "/" version 2
+
+// const dd = new Date().toLocaleDateString("az-AZ")
+// let year = dd.slice(0,4)
+// let month = dd.slice(5,6)
+// let day = dd.slice(7,8)
+// let result = month + "/" + day + "/" + year 
+// console.log(result)
+
